ee9c0038e533b28b78a84d858b906666c8452cbc
unsignd <unsigndid@gmail.com>
2024-06-26 00:46:41 -0700

    define getPalette, getTheme functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,14 @@
+import { palette, theme } from '../configs';
+import { PaletteType, ThemeType } from '../types';
+
 export function toRem(px: number): number {
   return px / 16;
+}
+
+export function getPalette(): PaletteType {
+  return palette;
+}
+
+export function getTheme(): ThemeType {
+  return theme;
 }
