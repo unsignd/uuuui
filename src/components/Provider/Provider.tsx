d9b6edc496facf06c3249e23a053b1db1ecf3d18
unsignd <unsigndid@gmail.com>
2024-06-27 13:10:30 -0700

    edit initial color palette

--- src/components/Provider/Provider.tsx
+++ src/components/Provider/Provider.tsx
@@ -17,17 +17,17 @@ export default function Provider({
       primary: '',
 
       'base.100': '#ffffff',
-      'base.200': '',
-      'base.300': '#dadce0',
+      'base.200': '#e6e7eb',
+      'base.300': '#ced0d6',
       'base.400': '#a0a4ad',
-      'base.500': '#08090a',
+      'base.500': '#08080a',
     },
     dark: {
       primary: '#1f63eb',
 
       'base.100': '#08080a',
-      'base.200': '',
-      'base.300': '#202329',
+      'base.200': '#181a1f',
+      'base.300': '#292c33',
       'base.400': '#4d525c',
       'base.500': '#ffffff',
     },
