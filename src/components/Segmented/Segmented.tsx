7f08ab606e5d6d915a9f8019838eb96ee4a9e88f
unsignd <unsigndid@gmail.com>
2024-06-29 01:50:59 -0700

    code basic Segmented component structure

--- src/components/Segmented/Segmented.tsx
+++ src/components/Segmented/Segmented.tsx
@@ -1,10 +1,129 @@
-import { HTMLAttributes } from 'react';
+import { HTMLAttributes, ReactNode } from 'react';
 import styled from 'styled-components';
+import {
+  BorderCurveType,
+  ColorType,
+  PriorityType,
+  ThemeType,
+} from '../../types';
+import { toRem } from '../../utils';
+import { Sans } from '../../global';
+import { useTheme } from '../../contexts';
+import Icon from '../Icon';
 
-export interface SegmentedProps extends HTMLAttributes<HTMLDivElement> {}
+export interface SegmentedProps extends HTMLAttributes<HTMLDivElement> {
+  options: {
+    [key: string]: {
+      disabled?: boolean;
 
-const Wrapper = styled.div``;
+      text?: string;
+      icon?: React.FC<React.SVGProps<SVGSVGElement>>;
+      color?: ColorType;
 
-export default function Segmented({ ...attr }: SegmentedProps) {
-  return <Wrapper {...attr}></Wrapper>;
+      curve?: BorderCurveType;
+
+      onClick?: (
+        event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+      ) => void;
+    };
+  };
+
+  selection?: string;
+}
+
+const Wrapper = styled.div`
+  height: ${toRem(40)}rem;
+
+  display: flex;
+`;
+
+const ButtonContainer = styled.button<{
+  $children?: ReactNode;
+  $icon?: React.FC<React.SVGProps<SVGSVGElement>>;
+
+  $active?: boolean;
+  $disabled: boolean;
+
+  $curve: BorderCurveType;
+
+  $theme: ThemeType;
+}>`
+  width: ${(props) =>
+    !props.$children && props.$icon ? `${toRem(40)}rem` : 'auto'};
+  height: ${toRem(40)}rem;
+
+  padding: 0 ${(props) => (!props.$children && props.$icon ? 0 : toRem(14))}rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: ${toRem(7)}rem;
+
+  color: ${(props) => props.theme[props.$theme]['base.500']};
+  background-color: ${(props) =>
+    props.$active
+      ? props.theme[props.$theme]['base.200']
+      : props.theme[props.$theme]['base.100']};
+
+  border: none;
+  border-radius: ${(props) =>
+    ({
+      medium: toRem(10),
+      large: toRem(20),
+    }[props.$curve])}rem;
+
+  transition: scale 100ms ease-in-out;
+
+  overflow: hidden;
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  opacity: ${(props) => (props.$disabled ? 0.4 : 1)};
+  cursor: ${(props) => (props.$disabled ? 'not-allowed' : 'pointer')};
+
+  &:hover {
+    background-color: ${(props) =>
+      props.$disabled ? undefined : props.theme[props.$theme]['base.200']};
+  }
+
+  &:active {
+    scale: 0.96;
+  }
+
+  & svg {
+    height: ${toRem(16)}rem;
+
+    flex-shrink: 0;
+  }
+`;
+
+const Text = styled(Sans)`
+  font-size: ${toRem(14)}rem;
+`;
+
+export default function Segmented({
+  options,
+  selection,
+  ...attr
+}: SegmentedProps) {
+  const { theme } = useTheme();
+
+  return (
+    <Wrapper {...attr}>
+      {Object.keys(options).map((key) => (
+        <ButtonContainer
+          key={key}
+          $children={options[key].text}
+          $icon={options[key].icon}
+          $active={selection === key}
+          $disabled={options[key].disabled ?? false}
+          $curve={options[key].curve ?? 'medium'}
+          $theme={theme}
+        >
+          {options[key].icon ? <Icon icon={options[key].icon} /> : undefined}
+          {options[key].text ? <Text>{options[key].text}</Text> : undefined}
+        </ButtonContainer>
+      ))}
+    </Wrapper>
+  );
 }
