c806223cd4782b7c0012bbee331f5ac7c5399a2d
unsignd <unsigndid@gmail.com>
2024-06-29 14:34:54 -0700

    code temp Table component

--- src/layouts/Table/Table.tsx
+++ src/layouts/Table/Table.tsx
@@ -1,11 +1,26 @@
+import { createColumnHelper } from '@tanstack/react-table';
 import styled from 'styled-components';
 
-interface TableProps {
-  data: Object[];
+interface TableProps<T> {
+  data: T[];
 }
 
 const Wrapper = styled.div``;
 
-export default function Table({ data: [] }: TableProps) {
-  return <Wrapper></Wrapper>;
+export default function Table<T extends { [key: string]: any }>({
+  data = [],
+}: TableProps<T>) {
+  const columnHelper = createColumnHelper<T>();
+
+  const columns =
+    data.length > 0
+      ? Object.keys(data[0]).map((key) =>
+          columnHelper.accessor(key as keyof T, {
+            header: () => key,
+            cell: (info) => info.renderValue(),
+          })
+        )
+      : [];
+
+  return <Wrapper>{data}</Wrapper>;
 }
