4bdd7e69af4a1957be0a4fac3d73942a6a4e69f8
unsignd <unsigndid@gmail.com>
2024-06-29 18:17:35 -0700

    add flex props on Texts' styles

--- src/global/Mono.tsx
+++ src/global/Mono.tsx
@@ -4,6 +4,9 @@ import { toRem } from '../utils';
 const Mono = styled.p`
   margin: 0;
 
+  display: flex;
+  align-items: center;
+
   font-family: Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace;
   text-overflow: ellipsis;
   letter-spacing: ${toRem(-0.35)}rem;
