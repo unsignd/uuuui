759640510a1663a67449ee71508afb7824270318
unsignd <unsigndid@gmail.com>
2024-07-01 07:16:45 -0700

    dark/light theme switch on storybook preview.tsx (temp)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,18 +2,37 @@ import React from 'react';
 import type { Preview } from '@storybook/react';
 
 import centered from '@storybook/addon-centered/react';
+import { useDarkMode } from 'storybook-dark-mode';
+
 import Provider from '../src/components/Provider';
 
 const preview: Preview = {
   decorators: [
     centered,
-    (Story) => (
-      <Provider>
-        <Story />
-      </Provider>
-    ),
+    (Story) => {
+      if (useDarkMode()) {
+        return (
+          <Provider theme="dark">
+            <Story />
+          </Provider>
+        );
+      } else {
+        return (
+          <Provider theme="light">
+            <Story />
+          </Provider>
+        );
+      }
+    },
   ],
   parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'light', value: '#ffffff' },
+        { name: 'dark', value: '#333333' },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
