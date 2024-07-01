079b85f030b233b199a0a373d9af22f04efb3879
unsignd <unsigndid@gmail.com>
2024-07-01 02:17:09 -0700

    fix Sans overflow bug

--- src/global/Sans.tsx
+++ src/global/Sans.tsx
@@ -3,9 +3,6 @@ import styled from 'styled-components';
 const Sans = styled.p`
   margin: 0;
 
-  display: flex;
-  align-items: center;
-
   font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
   text-overflow: ellipsis;
 
