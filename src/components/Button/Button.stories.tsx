b5a598427c02045630ac5ff026ce5b4b3e44fac6
unsignd <unsigndid@gmail.com>
2024-07-01 01:45:58 -0700

    code Button component stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -5,10 +5,31 @@ import Button from './Button';
 const meta: Meta<typeof Button> = {
   title: 'Components/Button',
   component: Button,
+  argTypes: {
+    children: {
+      control: 'text',
+      description: 'Overwritten description',
+    },
+  },
 };
 
-export const Primary = () => <Button>Primary</Button>;
-export const Secondary = () => <Button>Secondary</Button>;
-export const Clickable = () => <Button>Clickable</Button>;
+export const Low_Priority: Meta<typeof Button> = {
+  args: {
+    priority: 'low',
+    children: 'Button',
+  },
+};
+export const Medium_Priority: Meta<typeof Button> = {
+  args: {
+    priority: 'medium',
+    children: 'Button',
+  },
+};
+export const High_Priority: Meta<typeof Button> = {
+  args: {
+    priority: 'high',
+    children: 'Button',
+  },
+};
 
 export default meta;
